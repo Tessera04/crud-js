2cdb583c02c8cbf014334b2b3fd1d8cf0741cf72
Tessera04 <matute_11@hotmail.com.ar>
2022-11-23 20:25:03 -0300

    Refactorizando responsabilidades

--- services/client-service.js
+++ services/client-service.js
@@ -1,52 +1,6 @@
-const crearNuevaLinea = (nombre, email) => {
-    const linea = document.createElement("tr");
-    const contenido = `
-            <td class="td" data-td>${nombre}</td>
-            <td>${email}</td>
-            <td>
-              <ul class="table__button-control">
-                <li>
-                  <a
-                    href="../screens/editar_cliente.html"
-                    class="simple-button simple-button--edit"
-                    >Editar</a
-                  >
-                </li>
-                <li>
-                  <button
-                    class="simple-button simple-button--delete"
-                    type="button"
-                  >
-                    Eliminar
-                  </button>
-                </li>
-              </ul>
-            </td>`;
-    
-    linea.innerHTML = contenido;
-    return linea;
-}
-
-const table = document.querySelector("[data-table]");
-
-
-//abrir http (metodo, url)
-
-//CRUD (Create Read Update Delete)
-//Create - POST
-//Read - GET
-//Update - PUT/PATCH
-//Delete - DELETE
-
-//Fetch API
 const listaCLientes = () => 
     fetch('http://localhost:3000/perfil').then((respuesta) => respuesta.json());
 
-listaCLientes().then((data) => {
-    data.forEach((perfil) => {
-        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-        table.appendChild(nuevaLinea);
-    });
-})
-.catch((error) => alert("Ocurrio un error"));
-
+export const clientServices = {
+    listaCLientes,
+};
